af0b294725b16efc4a447d0866d32d27bf913cc2
jmihalsky <jmihalsky@gmail.com>
2018-10-27 00:42:01 -0700

    added inquirer menu options and ability to handle command criteria not in quotes

--- liri.js
+++ liri.js
@@ -3,9 +3,9 @@ require("dotenv").config({path: "./keys.env"});
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var request = require("request");
+var inquirer = require("inquirer");
 var dateFormat = require("dateformat");
-// var APP_ID = keys.band_town.api_key;
-// var bandsintown = require("bandsintown")(APP_ID);
+
 var fs = require("fs");
 
 
@@ -13,15 +13,17 @@ var api_string = "";
 
 if(process.argv[2] == "concert-this")
 {
+    remove_spaces(1,process.argv);
     band_town();
 }
 else if(process.argv[2] == "spotify-this-song")
 {
+    remove_spaces(1,process.argv);
     spotify_api();
 }
 else if(process.argv[2] == "movie-this")
 {
-    console.log("movie API");
+    remove_spaces(1,process.argv);
     omdb_api();
 }
 else if(process.argv[2] == "do-what-it-says")
@@ -31,29 +33,118 @@ else if(process.argv[2] == "do-what-it-says")
 else
 {
     console.log("inquirer menu will appear");
+    
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Select one of the Liribot function to perform: ",
+            choices: ["Bands in town search","Spotify Artist, Album, Song Search","OMDB Movie Search","Do as I say option"],
+            name: "main_menu_choices"
+        }
+
+    ]).then(function(MainMenuResponse){
+        switch(MainMenuResponse.main_menu_choices)
+        {
+            case "Bands in town search":
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "Enter the band you would like concert information on: ",
+                        name: "band"
+                    }
+                ]).then(function(BandsOption){
+                    remove_spaces(2,BandsOption.band);
+                    band_town();
+                });
+                break;
+            case "Spotify Artist, Album, Song Search":
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "Enter a song, album, or band to search on Spotify.",
+                        name: "spotify"
+                    }
+                ]).then(function(SpotifyOption){
+                    remove_spaces(2,SpotifyOption.spotify);
+                    spotify_api();
+                });
+                break;
+            case "OMDB Movie Search":
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        message: "Enter a movie title to find information on.",
+                        name: "OMDB"
+                    }
+                ]).then(function(MovieOption){
+                    remove_spaces(2,MovieOption.OMDB);
+                    omdb_api();
+                });
+                break;
+            case "Do as I say option":
+                console.log("future code");
+                break;
+            default:
+                console.log("help options");
+        }
+    });
 }
 
-function remove_spaces(arg_string){
-    var arg_arry = arg_string.split(" ");
-    for(var i = 0; i < arg_arry.length; i++)
+function remove_spaces(cmdmnu,process_arry){
+    if(cmdmnu == 1)
     {
-        if(i == 0 )
+        if (process_arry.length < 5)
         {
-            api_string = arg_arry[i];
+            var arg_arry = process_arry[3].split(" ");
+            for(var i = 0; i < arg_arry.length; i++)
+            {
+                if(i == 0 )
+                {
+                    api_string = arg_arry[i];
+                }
+                else
+                {
+                    api_string += "+" + arg_arry[i];
+                }
+            }
         }
         else
         {
-            api_string += "+" + arg_arry[i];
+            for(var i = 3; i < process_arry.length; i++)
+            {
+                if(i == 3)
+                {
+                    api_string = process_arry[i];
+                }
+                else
+                {
+                    api_string += "+" + process_arry[i];
+                }
+            }
         }
+    }
+    else if (cmdmnu == 2)
+    {
+        var arg_arry = process_arry.split(" ");
+        for (var i = 0; i < arg_arry.length; i++)
+        {
+            if (i == 0 )
+            {
+                api_string = arg_arry[i];
+            }
+            else
+            {
+                api_string += "+" + arg_arry[i];
+            }
+        }
+
     }
 }
 
 function spotify_api(){
-    remove_spaces(process.argv[3]);
-    console.log(api_string);
-
+    
+    
     var spotify_keys = keys.spotify;
-    console.log(spotify_keys);
 
     var spotify = new Spotify(keys.spotify);
     
@@ -82,7 +173,7 @@ function spotify_api(){
 }
 
 function band_town(){
-    remove_spaces(process.argv[3]);
+    // remove_spaces(process.argv);
 
     var band_api_url = "https://rest.bandsintown.com/artists/" + api_string + "/events?app_id=" + keys.band_town.api_key;
     console.log(band_api_url);
@@ -115,7 +206,7 @@ function band_town(){
 }
 
 function omdb_api(){
-    remove_spaces(process.argv[3]);
+    // remove_spaces(process.argv);
 
     var movie_api_url = "http://www.omdbapi.com/?t=" + api_string + "&y=&plot=short&apikey=trilogy";
 
